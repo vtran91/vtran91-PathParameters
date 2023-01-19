12117608bbda9c587bc84f101a45ca821fab039a
tedbeast <theodore.balashov@revature.com>
2023-01-19 14:01:59 -0500

    added useful comment

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -3,23 +3,24 @@
 import io.javalin.Javalin;
 
 public class JavalinSingleton {
-
+    /**
+     * Assignment: retrieve the variable "first" from the path parameter and send it in the response body. Produce
+     * the response using:
+     *      ctx.result(StringVariable);
+     *
+     * You will not need to run app.start in this method. The test cases, or main method, will do this for you - this
+     * method only needs to return a properly configured Javalin Server, represented by the 'app' object created below.
+     *
+     * Please refer to the "PathParameters.MD" file.
+     */
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
-        
-        /**
-         * Assignment: retrieve the variable "first" from the path parameter and send it in the response body. Produce
-         * the response using:
-         *      ctx.result(StringVariable);
-         * 
-         * Note: Please refer to the "PathParameters.MD" file for more assistance if needed.
-         */
+
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
 
         });
-
 
         return app;
     }
